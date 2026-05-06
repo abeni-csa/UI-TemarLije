a5818da6e5cd1a392968bb57d450be09067191c2
abenezer@csa <abenezerhaymanot@gmail.com>
2026-05-06 08:02:57 +0300

    costom shimmer effect

---
+++ lib/common/widgets/shimmers/shimmer.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+import 'package:ui_temarlije/utils/constants/colors.dart';
+import 'package:ui_temarlije/utils/helpers/helper_functions.dart';
+
+class TemarLijeShimmerEffect extends StatelessWidget {
+  const TemarLijeShimmerEffect({
+    super.key,
+    required this.width,
+    required this.height,
+    this.radius = 15,
+    this.color,
+  });
+
+  final double width, height, radius;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = TemarLijeHelperFunctions.isDarkMode(context);
+    return Shimmer.fromColors(
+      baseColor: dark ? Colors.grey[850]! : Colors.grey[300]!,
+      highlightColor: dark ? Colors.grey[700]! : Colors.grey[100]!,
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color:
+              color ??
+              (dark ? TemarLijeColors.darkerGrey : TemarLijeColors.white),
+          borderRadius: BorderRadius.circular(radius),
+        ),
+      ),
+    );
+  }
+}
