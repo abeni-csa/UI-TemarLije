6e939afa20f9120d7178ec19f11f87c98987f917
abenezer@csa <abenezerhaymanot@gmail.com>
2026-05-06 11:00:49 +0300

    (feat) middileware init

---
+++ lib/routes/routes_middilware.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ui_temarlije/routes/routes.dart';
+
+class TemarLijeMiddlware extends GetMiddleware {
+  RouteSettings? redirect(String? route) {
+    final isAuteticated = false;
+    return isAuteticated
+        ? null
+        : const RouteSettings(name: TemarLijeStaticPageRoutes.logIn);
+  }
+}
