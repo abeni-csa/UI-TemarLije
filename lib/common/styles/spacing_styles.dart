e89c94495b2b3c750977b06cedfcd8680b0922fb
abenezer@csa <abenezerhaymanot@gmail.com>
2026-05-07 08:31:55 +0300

    common styings

---
+++ lib/common/styles/spacing_styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:ui_temarlije/utils/constants/sizes.dart';
+
+class TemarLijeSpacingStyle {
+  static const EdgeInsetsGeometry paddingWithAppBarHeight = EdgeInsets.only(
+    top: TemarLijeSizes.appBarHeight,
+    left: TemarLijeSizes.defaultSpace,
+    bottom: TemarLijeSizes.defaultSpace,
+    right: TemarLijeSizes.defaultSpace,
+  );
+}
