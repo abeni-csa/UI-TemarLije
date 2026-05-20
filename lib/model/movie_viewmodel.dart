0a654aa920fd4bc25d6b292af52f9c3a940653ca
abenezer@csa <abenezerhaymanot@gmail.com>
2026-05-20 10:27:41 +0300

    Final Momen Before Distructions

---
+++ lib/model/movie_viewmodel.dart
@@ -0,0 +1,10 @@
+import './movie.dart';
+
+class MovieViewModel {
+  late List<String> movieGenres;
+  List<Movie> trendingMovies = [];
+  List<Movie> topRatedMovies = [];
+  List<Movie> popularMovies = [];
+  List<Movie> nowPlayingMovies = [];
+  List<Movie> allMovies = [];
+}
